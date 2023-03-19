243ba2d3e1849ebbc81a4b41bff03eb8704a3fcd
Srisaravana M <saravanamuthaly@gmail.com>
2023-03-19 19:08:50 +0530

    updated arch

--- src/router/app/appRoutes.ts
+++ src/router/app/appRoutes.ts
@@ -1,16 +1,25 @@
 import type { RouteRecordRaw } from "vue-router";
+import { USERS_ALL } from "@/_backend/models/users/UserTypes";
 
-const PREFIX = "/app"
+const PREFIX = "/app";
 
 export const appRoutes: RouteRecordRaw[] = [
 	{
 		path: PREFIX,
 		name: "app/index",
 		component: () => import("@/pages/app/index.vue"),
+		meta: {
+			requiresAuth: true,
+			accessList: USERS_ALL,
+		},
 	},
 	{
 		path: PREFIX + "/user/manage",
 		name: "app/user/manage",
 		component: () => import("@/pages/app/user/manage.vue"),
+		meta: {
+			requiresAuth: true,
+			accessList: USERS_ALL,
+		},
 	},
 ];
